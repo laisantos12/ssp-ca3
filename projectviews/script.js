e9f7790b9274d5b5417b258dc01e524ee2cd80aa
laisantos12 <lsantospersonal@hotmail.com>
2021-12-30 18:26:26 +0000

    files moved and bootstrap deleted

--- projectviews/script.js
+++ projectviews/script.js
@@ -1,55 +1,7 @@
-function draw_table()
-{
-	$("#results").empty();
-	$.getJSONuncached = function (url)
-	{
-		return $.ajax(
-		{
-			url: url,
-			type: 'GET',
-			cache: false,
-			success: function (html)
-			{
-				$("#results").append(html);
-				select_row();
-			}
-		});
-	};
-	$.getJSONuncached("/get/html")
-};
-
-function select_row()
-{
-	$("#catalogTable tbody tr[id]").click(function ()
-	{
-		$(".selected").removeClass("selected");
-		$(this).addClass("selected");
-		var section = $(this).prevAll("tr").children("td[colspan='3']").length - 1;
-		var collectibles = $(this).attr("id") - 1;
-		delete_row(section, collectibles);
-	})
-};
-
-function delete_row(sec, col)
-{
-	$("#delete").click(function ()
-	{
-		$.ajax(
-		{
-			url: "/post/delete",
-			type: "POST",
-			data:
-			{
-				section: sec,
-				collectibles: col
-			},
-			cache: false,
-			success: setTimeout(draw_table, 1000)
-		})
-	})
-};
-
-$(document).ready(function ()
-{
-	draw_table();
-});
+/*!
+* Start Bootstrap - Bare v5.0.7 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
