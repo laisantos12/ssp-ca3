6119c1133d5f47ec7f428f2330debc403a2b4714
laisantos12 <lsantospersonal@hotmail.com>
2021-12-30 14:46:32 +0000

    testing server

--- index.js
+++ index.js
@@ -9,28 +9,28 @@ const   http = require('http'), //This module provides the HTTP server functiona
 const   router = express(), 
         server = http.createServer(router);
 
-router.use(express.static(path.resolve(__dirname,'views'))); //We serve static content from "views" folder
+router.use(express.static(path.resolve(__dirname,'projectviews'))); //We serve static content from "views" folder
 router.use(express.urlencoded({extended: true})); //We allow the data sent from the client to be encoded in a URL targeting our end point
 router.use(express.json()); //We include support for JSON
 
 
-// Function to read in XML file and convert it to JSON
-function XMLtoJSON(filename, cb) {
-        var filepath = path.normalize(path.join(__dirname, filename));
-        fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-          if (err) throw (err);
-          xml2js.parseString(xmlStr, {}, cb);
-        });
-    };
+// // Function to read in XML file and convert it to JSON
+// function XMLtoJSON(filename, cb) {
+//         var filepath = path.normalize(path.join(__dirname, filename));
+//         fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+//           if (err) throw (err);
+//           xml2js.parseString(xmlStr, {}, cb);
+//         });
+//     };
       
-      //Function to convert JSON to XML and save it
-    function JSONtoXML(filename, obj, cb) {
-        var filepath = path.normalize(path.join(__dirname, filename));
-        var builder = new xml2js.Builder();
-        var xml = builder.buildObject(obj);
-        fs.unlinkSync(filepath);
-        fs.writeFile(filepath, xml, cb);
-    };
+//       //Function to convert JSON to XML and save it
+//     function JSONtoXML(filename, obj, cb) {
+//         var filepath = path.normalize(path.join(__dirname, filename));
+//         var builder = new xml2js.Builder();
+//         var xml = builder.buildObject(obj);
+//         fs.unlinkSync(filepath);
+//         fs.writeFile(filepath, xml, cb);
+//     };
     
     router.get('/get/html', function(req, res) {
     
@@ -56,55 +56,55 @@ function XMLtoJSON(filename, cb) {
     
     });
     
-    router.post('/post/json', function (req, res) {
+    // router.post('/post/json', function (req, res) {
     
-        function appendJSON(obj) {
+    //     function appendJSON(obj) {
     
-            console.log(obj)
+    //         console.log(obj)
     
-            XMLtoJSON('PaddysCafe.xml', function (err, result) {
-                if (err) throw (err);
+    //         XMLtoJSON('PaddysCafe.xml', function (err, result) {
+    //             if (err) throw (err);
                 
-                result.menu.section[obj.sec_n].entry.push({'item': obj.item, 'price': obj.price});
+    //             result.menu.section[obj.sec_n].entry.push({'item': obj.item, 'price': obj.price});
     
-                console.log(JSON.stringify(result, null, "  "));
+    //             console.log(JSON.stringify(result, null, "  "));
     
-                JSONtoXML('PaddysCafe.xml', result, function(err){
-                    if (err) console.log(err);
-                });
-            });
-        };
+    //             JSONtoXML('PaddysCafe.xml', result, function(err){
+    //                 if (err) console.log(err);
+    //             });
+    //         });
+    //     };
     
-        appendJSON(req.body);
+    //     appendJSON(req.body);
     
-        res.redirect('back');
+    //     res.redirect('back');
     
-    });
+    // });
     
-    router.post('/post/delete', function (req, res) {
+    // router.post('/post/delete', function (req, res) {
     
-        function deleteJSON(obj) {
+    //     function deleteJSON(obj) {
     
-            console.log(obj)
+    //         console.log(obj)
     
-            XMLtoJSON('PaddysCafe.xml', function (err, result) {
-                if (err) throw (err);
+    //         XMLtoJSON('PaddysCafe.xml', function (err, result) {
+    //             if (err) throw (err);
                 
-                delete result.menu.section[obj.section].entry[obj.entree];
+    //             delete result.menu.section[obj.section].entry[obj.entree];
     
-                console.log(JSON.stringify(result, null, "  "));
+    //             console.log(JSON.stringify(result, null, "  "));
     
-                JSONtoXML('PaddysCafe.xml', result, function(err){
-                    if (err) console.log(err);
-                });
-            });
-        };
+    //             JSONtoXML('PaddysCafe.xml', result, function(err){
+    //                 if (err) console.log(err);
+    //             });
+    //         });
+    //     };
     
-        deleteJSON(req.body);
+    //     deleteJSON(req.body);
     
-        res.redirect('back');
+    //     res.redirect('back');
     
-    });
+    // });
     
     server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
         const addr = server.address();
