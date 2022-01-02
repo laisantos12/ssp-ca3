d4840ab96bfab4be63444a17f17bd4f2d6807646
laisantos12 <lsantospersonal@hotmail.com>
2022-01-02 18:33:33 +0000

    converting xml to json and json to xml

--- index.js
+++ index.js
@@ -14,23 +14,23 @@ router.use(express.urlencoded({extended: true})); //We allow the data sent from 
 router.use(express.json()); //We include support for JSON
 
 
-// // Function to read in XML file and convert it to JSON
-// function XMLtoJSON(filename, cb) {
-//         var filepath = path.normalize(path.join(__dirname, filename));
-//         fs.readFile(filepath, 'utf8', function(err, xmlStr) {
-//           if (err) throw (err);
-//           xml2js.parseString(xmlStr, {}, cb);
-//         });
-//     };
+// Function to read in XML file and convert it to JSON
+function XMLtoJSON(filename, cb) {
+        var filepath = path.normalize(path.join(__dirname, filename));
+        fs.readFile(filepath, 'utf8', function(err, xmlStr) {
+          if (err) throw (err);
+          xml2js.parseString(xmlStr, {}, cb);
+        });
+    };
       
-//       //Function to convert JSON to XML and save it
-//     function JSONtoXML(filename, obj, cb) {
-//         var filepath = path.normalize(path.join(__dirname, filename));
-//         var builder = new xml2js.Builder();
-//         var xml = builder.buildObject(obj);
-//         fs.unlinkSync(filepath);
-//         fs.writeFile(filepath, xml, cb);
-//     };
+      //Function to convert JSON to XML and save it
+    function JSONtoXML(filename, obj, cb) {
+        var filepath = path.normalize(path.join(__dirname, filename));
+        var builder = new xml2js.Builder();
+        var xml = builder.buildObject(obj);
+        fs.unlinkSync(filepath);
+        fs.writeFile(filepath, xml, cb);
+    };
     
     router.get('/get/html', function(req, res) {
     
